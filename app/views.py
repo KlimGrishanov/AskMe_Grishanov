ecacf0cd03ae7ed22412532700cb566c4d8f68e4
Гришанов Клим <klim.grishanov@mail.ru>
2023-11-09 00:02:22 +0300

    core: add tags url

--- app/views.py
+++ app/views.py
@@ -1,13 +1,37 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from django.http import HttpResponse
 
+TAGS = [{
+    'id': 0,
+    'name': 'Python'
+}, {
+    'id': 1,
+    'name': 'Perl'
+}, {
+    'id': 2,
+    'name': 'Ruby'
+}]
+
+MEMBERS = [
+    {
+        'id': 0,
+        'name': 'Mr Horse'
+    }, {
+        'id': 1,
+        'name': 'Mrs Horse'
+    }, {
+        'id': 2,
+        'name': 'Mr Sheep'
+    }
+]
 
 QUESTIONS = [
         {
             'id': i,
             'title': f'Question {i}',
-            'content': f'Long lorem ipsum {i}'
+            'content': f'Long lorem ipsum {i}',
+            'tag': TAGS[i % 3].get('name'),
+            'tagId': i % 3
         } for i in range(100)
     ]
 
@@ -28,7 +52,7 @@ def index(request):
     if page < 1 or page > len(QUESTIONS) / PER_PAGE:
         return render(request, 'index.html')
 
-    return render(request, 'index.html', {'questions': paginate(QUESTIONS, page)})
+    return render(request, 'index.html', {'questions': paginate(QUESTIONS, page), 'tags': TAGS, 'members': MEMBERS})
 
 
 def hot(request):
@@ -41,11 +65,16 @@ def hot(request):
     if page < 1 or page > len(QUESTIONS) / PER_PAGE:
         return render(request, 'hot.html')
 
-    return render(request, 'hot.html', {'questions': paginate(QUESTIONS, page)})
+    return render(request, 'hot.html', {'questions': paginate(QUESTIONS, page), 'tags': TAGS, 'members': MEMBERS})
 
 
 def tag(request):
-    page = 1
+    page = request.GET.get('page', 1)
+    tag = request.GET.get('tag', 0)
+    questions = []
+    for i in range(0, len(QUESTIONS)):
+        if QUESTIONS[i].get('tag') == TAGS[int(tag)].get('name'):
+            questions.append(QUESTIONS[i])
     # page = request.GET.get('page', 1)
     # try:
     #     page = int(page)
@@ -55,7 +84,7 @@ def tag(request):
     # if page < 1 or page > len(QUESTIONS) / PER_PAGE:
     #     return render(request, 'tag.html')
 
-    return render(request, 'tag.html', {'questions': paginate(QUESTIONS, page)})
+    return render(request, 'tag.html', {'questions': paginate(questions, page), 'tags': TAGS, 'members': MEMBERS, 'curTag': TAGS[int(tag)].get('name')})
 
 
 def settings(request):
@@ -80,19 +109,19 @@ def question(request, question_id):
             'content': f'Long lorem ipsum {i}'
         } for i in range(100)
     ]
-    return render(request, 'question.html', {'question': question, 'answers': paginate(answers, page)})
+    return render(request, 'question.html', {'question': question, 'answers': paginate(answers, page), 'tags': TAGS, 'members': MEMBERS})
 
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'tags': TAGS, 'members': MEMBERS})
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    return render(request, 'signup.html', {'tags': TAGS, 'members': MEMBERS})
 
 
 def ask(request):
-    return render(request, 'ask.html')
+    return render(request, 'ask.html', {'tags': TAGS, 'members': MEMBERS})
 
 def logout(request):
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'tags': TAGS, 'members': MEMBERS})
